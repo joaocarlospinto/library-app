b6d305d4829b56449d5fb6fe7dcf889a51aafb00
joaocpinto <joao.pinto@ymail.com>
2023-08-28 16:03:54 +0100

    Commit

---
+++ src/main/java/com/pluralsight/model/Book.java
@@ -0,0 +1,31 @@
+package com.pluralsight.model;
+
+public class Book {
+    private Long id;
+    private String title;
+    private int rating;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
